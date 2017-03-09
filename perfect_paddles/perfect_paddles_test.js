5ae7b76c00f02b58982d292faefbd6c51e94149a
Tina Holly <tinacious.design@gmail.com>
2017-03-08 20:52:16 -0500

    better tests for media queries, fontawesome

--- perfect_paddles/perfect_paddles_test.js
+++ perfect_paddles/perfect_paddles_test.js
@@ -9,18 +9,18 @@ const expect = Chai.expect;
 
 
 describe('Perfect Paddles', () => {
-  
+
   describe('HTML', () => {
 
     let $;
     JSDom();
 
-    let htmlBody = null; 
+    let htmlBody = null;
     before(() => {
       $ = require('jquery');
 
       Assert.ok(process.env.HTML, 'No HTML path included');
-      
+
       const response = Request('GET', process.env.HTML);
       document.body.innerHTML = response.getBody('utf8');
     });
@@ -36,19 +36,19 @@ describe('Perfect Paddles', () => {
 
       expect(viewport).to.contain('width=device-width');
     });
-    
+
     it('has paragraphs', () => {
 
       Assert.ok($('p').length);
     });
 
     it('does not use <br /> tags to create whitespace', () => {
-      
+
       Assert.equal($('br').length, 0, 'Whitespace should be created with CSS margins, padding, etc. rather than <br /> tags');
     });
 
     it('has only one <h1>', () => {
-      
+
       Assert.equal($('h1').length, 1);
     });
 
@@ -59,7 +59,7 @@ describe('Perfect Paddles', () => {
     });
 
     it('info@perfectpaddles.com link is a valid mailto link', () => {
-      
+
       const email = 'info@perfectpaddles.com';
       const mailLink = $('a[href*="mailto:"]');
 
@@ -68,7 +68,7 @@ describe('Perfect Paddles', () => {
     });
 
     it('includes a CSS reset', () => {
-      
+
       Assert.ok($('link[href*="reset.css"]').length);
     });
 
@@ -82,25 +82,25 @@ describe('Perfect Paddles', () => {
     });
 
     it('social media links use FontAwesome', () => {
-      
-      Assert.equal($('.fa-facebook').length, 1, 'Facebook icon not found');
-      Assert.equal($('.fa-instagram').length, 1, 'Instagram icon not found');
-      Assert.equal($('.fa-twitter').length, 1, 'Twitter icon not found');
-      Assert.equal($('.fa-pinterest').length, 1, 'Pinterest icon not found');
-      Assert.equal($('.fa-google-plus').length, 1, 'Google+ icon not found');
+
+      Assert.equal($('[class*="fa-facebook"]').length, 1, 'Facebook icon not found');
+      Assert.equal($('[class*="fa-instagram"]').length, 1, 'Instagram icon not found');
+      Assert.equal($('[class*="fa-twitter"]').length, 1, 'Twitter icon not found');
+      Assert.equal($('[class*="fa-pinterest"]').length, 1, 'Pinterest icon not found');
+      Assert.equal($('[class*="fa-google-plus"]').length, 1, 'Google+ icon not found');
     });
 
     it('incorporates a list-based navigation', () => {
 
       const menuLinks = $('ul').first().find('li a');
-      
+
       Assert.equal(menuLinks.length, 3, 'does not contain 3 links');
 
       menuLinks.each((i, link) => {
         const linkText = $(link).text().toLowerCase().trim();
 
         Assert.ok(
-          ['about', 'rentals', 'location'].indexOf(linkText) >= 0, 
+          ['about', 'rentals', 'location'].indexOf(linkText) >= 0,
           `Link ${linkText} not in the list.`
         );
       });
@@ -119,7 +119,7 @@ describe('Perfect Paddles', () => {
     let cssString = null;
 
     before(() => {
-      
+
       const response = Request('GET', process.env.CSS);
       cssString = response.getBody('utf8');
     });
@@ -128,11 +128,11 @@ describe('Perfect Paddles', () => {
 
       expect(cssString).to.contain('background');
     });
-    
+
     it('alters visual display of text', () => {
 
       ['font-family', 'color'].forEach((style) => {
-        
+
         expect(cssString).to.contain(style);
       });
     });
@@ -141,7 +141,7 @@ describe('Perfect Paddles', () => {
 
       expect(cssString).to.contain('@font-face');
     });
-    
+
     it('uses some CSS3 properties', () => {
 
       const propsNotIncl = [];
@@ -161,14 +161,19 @@ describe('Perfect Paddles', () => {
     });
 
     it('has required media queries for mobile-first responsive design', () => {
-
       try {
         expect(cssString).to.contain('min-width: 600px');
-        expect(cssString).to.contain('min-width: 1140px');
       }
       catch (e) {
         // whitespace :(
         expect(cssString).to.contain('min-width:600px');
+      }
+
+      try {
+        expect(cssString).to.contain('min-width: 1140px');
+      }
+      catch (e) {
+        // whitespace :(
         expect(cssString).to.contain('min-width:1140px');
       }
     });
